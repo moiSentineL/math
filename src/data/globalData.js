af42638d3494dd0d4c870b0f0dc1a3ebf13fad26
moiSentineL <moiSentineL@users.noreply.github.com>
2025-06-05 10:45:32 +0000

    deploy: 92d8fef5b9ac1d456f327badd5477843c06d8aef

--- src/data/globalData.js
+++
@@ -1,8 +0,0 @@
-// Note: changing this requires restarting the server to reflect the changes
-module.exports = {
-  navigation: [
-    { title: "about", path: "/", fullMatch: true },
-    { title: "posts", path: "/posts", fullMatch: false },
-    { title: "rss", path: "/feed.xml" },
-  ],
-};
